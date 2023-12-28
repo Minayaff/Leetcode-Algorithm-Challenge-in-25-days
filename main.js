47ddbabd843b57a5c2ef614f1e4496384c67c1d5
Minayaff <minayaff@code.edu.az>
2023-12-28 15:57:09 +0400

    today algorithm

--- main.js
+++ main.js
@@ -119,10 +119,10 @@
 //         let left = 0;
 //         let right = nums.length - 1;
 //         let idx = -1;
-      
+
 //         while (left <= right) {
 //             const mid = Math.floor((left + right) / 2);
-            
+
 //             if (nums[mid] > target) {
 //                 right = mid - 1;
 //             } else if (nums[mid] < target) {
@@ -136,13 +136,13 @@
 //                 }
 //             }
 //         }
-      
+
 //         return idx;
 //     };
-    
+
 //     const left = binarySearch(nums, target, true);
 //     const right = binarySearch(nums, target, false);
-    
+
 //     return [left, right];    
 // };
 
@@ -170,7 +170,7 @@
 //             right--;
 //         }
 //     }
-   
+
 //     return "NO";
 // }
 
@@ -194,21 +194,102 @@
 
 // 3. Longest Substring Without Repeating Characters
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
-var lengthOfLongestSubstring = function(s) {
-    var map=new Map();
-    var longestStr=0;
-    var left=0;
-    var right=0;
-    while(right<s.length){
-         var letter=s[right]
-         if(!map.has(letter)){
-             map.set(letter);
-             longestStr=Math.max(longestStr,map.size);
-             right++;
-         }else{
-             map.delete(s[left]);
-             left++;
-         }
+// var lengthOfLongestSubstring = function(s) {
+//     var map=new Map();
+//     var longestStr=0;
+//     var left=0;
+//     var right=0;
+//     while(right<s.length){
+//          var letter=s[right]
+//          if(!map.has(letter)){
+//              map.set(letter);
+//              longestStr=Math.max(longestStr,map.size);
+//              right++;
+//          }else{
+//              map.delete(s[left]);
+//              left++;
+//          }
+//     }
+//     return longestStr
+// };
+
+//https://leetcode.com/problems/logger-rate-limiter/description/?envType=featured-list&envId=top-google-questions%3FenvType%3Dfeatured-list&envId=top-google-questions
+//359. Logger Rate Limiter
+// this.map = new Map()  
+// Logger.prototype.shouldPrintMessage = function(timestamp, message) {
+//     if (this.map.has(message)) {        
+//         if (timestamp < this.map.get(message) + 10) {
+//             return false
+//         }
+//     }
+
+//     this.map.set(message, timestamp)
+//     return true  
+// };
+
+// Logger.prototype.shouldPrintMessage(["Logger", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage"],[[], [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11, "foo"]])
+
+//https://leetcode.com/problems/house-robber/description/
+var rob = function (nums) {
+    if (nums.length == 1) return nums[0];
+    let n = nums.length;
+
+    let p1 = nums[0]; //2
+    let p2 = Math.max(p1, nums[1]);
+    let max = 0;
+    for (var i = 2; i < n; i++) {
+        max = Math.max(p1 + num[i], p2); 10, 11
+        p1 = p2;
+        p2 = max;
     }
-    return longestStr
+
+};
+
+// var nums = [2,7,9,3,1]
+// rob(nums)
+
+// https://leetcode.com/problems/climbing-stairs/description/
+var climbStairs = function (n) {
+
+    if (n == 1) {
+        return 1;
+    }
+    var dp = [0];
+    dp[1] = 1;
+    dp[2] = 2;
+    for (var i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
+
+climbStairs(5);
+
+
+// https://leetcode.com/problems/edit-distance/description/
+
+  var minDistance = function(word1, word2) {
+    const m = word1.length;
+    const n = word2.length;
+    const dp = new Array(m+1).fill(null).map(() => new Array(n+1).fill(null));
+    
+    for (let i = 0; i <= m; i++) {
+        dp[i][0] = i;
+    }
+    
+    for (let j = 0; j <= n; j++) {
+        dp[0][j] = j;
+    }
+    
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (word1[i-1] === word2[j-1]) {
+                dp[i][j] = dp[i-1][j-1];
+            } else {
+                dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1;
+            }
+        }
+    }
+    
+    return dp[m][n];
 };
